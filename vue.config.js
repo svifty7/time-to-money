c6b36e31b9edd5e8b93d38db484282a48831a6db
svifty7 <svifty7@gmail.com>
2021-06-03 20:40:06 +0300

    fix deploy config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/time-to-money/'
+        : '/',
     outputDir: 'public_html',
+    filenameHashing: true,
+    runtimeCompiler: true,
     productionSourceMap: false
 }
