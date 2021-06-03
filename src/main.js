c3184b104ad31c3cf86190246cbe9d5b43e10915
svifty7 <svifty7@gmail.com>
2021-06-03 20:26:37 +0300

    remove axios

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
-import axios from 'axios';
 import ElementUI from 'element-ui'
 import App from './App.vue'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
 
-Vue.config.productionTip = false
-
-Vue.prototype.$axios = axios;
+Vue.config.productionTip = false;
 
 new Vue({
     render: h => h(App)
